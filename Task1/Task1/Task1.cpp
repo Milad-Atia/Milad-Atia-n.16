bc16288e4b4c1d6a37124e47014e78015829abde
Milad-Atia <miloatia682007@gmail.com>
2025-05-18 13:31:13 +0300

    Task1.cpp

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -7,17 +7,14 @@ int main()
     int n; //length of the garden
     int m; //width of the garden
     int a; //the side length of the square
-    cout << "Enter the length of the garden:" << endl;
     cin >> n;
-    cout << "Enter the width of the garden:" << endl;
     cin >> m;
-    cout << "Enter the side length of the square" << endl;
     cin >> a; 
     //adding (a - 1) ensures we round up the division to cover any remainder.
     int h = (n + a - 1) / a; //number of flower beds in horizontal direction
     int v = (m + a - 1) / a; //number of flower beds in vertical direction
     int total_beds = h * v;
-    cout << "The total number of flower beds required: " << total_beds << endl;
+    cout << total_beds << endl;
 
 }
 
