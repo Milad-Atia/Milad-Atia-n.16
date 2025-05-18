784c7505f6c4c3723e8200931ff43d1133250bdb
Milad-Atia <miloatia682007@gmail.com>
2025-05-18 13:32:15 +0300

    Task2.cpp

--- Task2'/Task2'/Task2'.cpp
+++ Task2'/Task2'/Task2'.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main()
 {
 	int n;
-	cout << "Enter the number of projects: ";
 	cin >> n;
 	
 	int count = 0;
@@ -11,11 +10,8 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		int a, b, c;
-		cout << "Vote for each project: ";
 		cin >> a;
-		cout << " ";
 		cin >> b;
-		cout << " ";
 		cin >> c;
 		if (a+b+c>=2)
 		{
@@ -25,5 +21,5 @@ int main()
 		
 	}
 	
-	cout << "Accepted projects: " << count;
+	cout << count;
 }
